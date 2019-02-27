0935064431805b6e2f839a5f62b003cac531a045
KP Kaiser <kirk@zothcorp.com>
2019-02-27 16:09:37 -0500

    Add min length, and add VP9, because we have it in the wild

--- splitter.py
+++ splitter.py
@@ -12,6 +12,9 @@ counter = 0
 
 for inference in b['inferences']:
     for i in range(len(inference['clock_segments'])):
+        length = 2
+        if inference['clock_segments'][i]['length'] >= 2:
+            length = inference['clock_segments'][i]['length']
         command = f"ffmpeg -i '{inference['filename']}' -ss {inference['clock_segments'][i]['start']} -t  {inference['clock_segments'][i]['length']} -c copy /downloads/slices/{counter:05d}.mp4"
         subprocess.call(shlex.split(command))
         counter += 1
