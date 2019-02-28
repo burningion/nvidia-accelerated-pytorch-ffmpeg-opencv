097172419a45c6b7e6103437d7f73c2ea7c6a72d
KP Kaiser <kirk@zothcorp.com>
2019-02-28 10:13:57 -0500

    For concat we need to have all snippets in the same codec

--- splitter.py
+++ splitter.py
@@ -10,11 +10,15 @@ b = req.json()
 
 counter = 0
 
+def get_sec(time_str):
+    h, m, s = time_str.split(':')
+    return int(h) * 3600 + int(m) * 60 + int(s)
+
 for inference in b['inferences']:
     for i in range(len(inference['clock_segments'])):
         length = 2
-        if inference['clock_segments'][i]['length'] >= 2:
-            length = inference['clock_segments'][i]['length']
-        command = f"ffmpeg -i '{inference['filename']}' -ss {inference['clock_segments'][i]['start']} -t  {inference['clock_segments'][i]['length']} -c copy /downloads/slices/{counter:05d}.mp4"
+        if get_sec(inference['clock_segments'][i]['length']) >= 2:
+            length = get_sec(inference['clock_segments'][i]['length'])
+        command = f"ffmpeg -i '{inference['filename']}' -ss {inference['clock_segments'][i]['start']} -t  {inference['clock_segments'][i]['length']} -f mp4 -vcodec libx264 -preset fast -profile:v main -acodec aac /downloads/slices/{counter:05d}.mp4"
         subprocess.call(shlex.split(command))
         counter += 1
