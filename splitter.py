56c173ca6ad8e9e73ce5b76af072dbe4a390dce3
KP Kaiser <kirk@zothcorp.com>
2019-03-05 12:11:10 -0500

    Move to uuid and fix length consistency

--- splitter.py
+++ splitter.py
@@ -2,6 +2,8 @@ import subprocess
 import requests
 import shlex
 
+import uuid
+
 import os
 
 scraperURL = 'http://' + os.environ['SCRAPERAPP_SERVICE_HOST'] + ':' + os.environ['SCRAPERAPP_SERVICE_PORT']
@@ -29,13 +31,22 @@ for inference in b['inferences']:
             continue
 
         length = getHrMinSec(10)
+
+        filename = uuid.uuid4()
         if get_sec(inference['clock_segments'][i]['length']) >= 10:
             length = getHrMinSec(10)
         else:
-            length = get_sec(inference['clock_segments'][i]['length'])
+            length = inference['clock_segments'][i]['length']
         if inference['filename'][-4:] == 'webm':
-            command = f"ffmpeg -hwaccel cuvid -c:v vp8_cuvid -i '{inference['filename']}' -ss {inference['clock_segments'][i]['start']} -t {length} -f mp4 -filter:v scale_npp=w=1280:h=720:format=yuv420p:interp_algo=lanczos -c:v h264_nvenc -preset slow -acodec aac -r 30 /downloads/slices/{counter:05d}.mp4 -hide_banner"
+            command = f"ffmpeg -hwaccel cuvid -c:v vp8_cuvid -i '{inference['filename']}' -ss {inference['clock_segments'][i]['start']} -t {length} -f mp4 -filter:v scale_npp=w=1280:h=720:format=yuv420p:interp_algo=lanczos -c:v h264_nvenc -preset slow -acodec aac -r 30 /downloads/slices/{filename}.mp4 -hide_banner"
         else:
-            command = f"ffmpeg -hwaccel cuvid -c:v h264_cuvid -i '{inference['filename']}' -ss {inference['clock_segments'][i]['start']} -t {length} -f mp4 -filter:v scale_npp=w=1280:h=720:format=yuv420p:interp_algo=lanczos -c:v h264_nvenc -preset slow -acodec aac -r 30 /downloads/slices/{counter:05d}.mp4 -hide_banner"
+            command = f"ffmpeg -hwaccel cuvid -c:v h264_cuvid -i '{inference['filename']}' -ss {inference['clock_segments'][i]['start']} -t {length} -f mp4 -filter:v scale_npp=w=1280:h=720:format=yuv420p:interp_algo=lanczos -c:v h264_nvenc -preset slow -acodec aac -r 30 /downloads/slices/{filename}.mp4 -hide_banner"
         subprocess.call(shlex.split(command))
+
+        snippet = {'filename': f'{filename}.mp4',
+                   'snippet_type': 'clock',
+                   'start': f"{inference['clock_segments'][i]['start']}",
+                   'duration': f"{length}",
+                   'video_id': f'{inference["video_id"]}'}
+        r = requests.post(f'{scraperURL}/snippets', json=snippet)
         counter += 1
